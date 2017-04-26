1b6c68c26bbb6f069a5de23ee3b076078b533b1c
Brendan Lindsay <brendan.g.lindsay@gmail.com>
2017-04-26 18:38:21 -0400

    initial commit

---
+++ wp-content/themes/transportation/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * transportation Theme Customizer
+ *
+ * @package transportation
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function transportation_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'transportation_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function transportation_customize_preview_js() {
+	wp_enqueue_script( 'transportation_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'transportation_customize_preview_js' );
